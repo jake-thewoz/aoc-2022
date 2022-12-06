aa938a3da6148c164d26ec9df83144fd783be208
Jakob <jake.thewoz@gmail.com>
2022-12-06 08:09:01 -0500

    Day 6 part one complete

---
+++ day_6.py
@@ -0,0 +1,16 @@
+import data_getter
+
+data = data_getter.get_data(6)
+
+def four_are_different(index):
+    bits = data[index:(index+4)]
+    for bit in bits:
+        if bits.count(bit) > 1:
+            return False
+    print(bits)
+    return index + 4
+
+for i in range(len(data)-5):
+    if four_are_different(i) != False:
+        print(f'The last bit of the start packet is at index (+1) {four_are_different(i)}.')
+        break
