18d4cdf78f4a9740e2e5264195199fbe18f05725
Jakob <jake.thewoz@gmail.com>
2022-12-14 13:27:10 -0500

    Got my vis working for day14, feels like a milestone

---
+++ day_14.py
@@ -0,0 +1,98 @@
+import data_getter
+
+data = data_getter.get_data(144).splitlines()
+
+# [print(line) for line in data]
+
+rock_paths = []
+all_x = []
+all_y = []
+visual = []
+
+for line in data:
+    endpoints = line.split(' -> ')
+    rock_paths.append(endpoints)
+    for point in endpoints:
+        x_values = int(point.split(',')[0])
+        y_values = int(point.split(',')[1])
+        all_x.append(x_values)
+        all_y.append(y_values)
+
+# print(all_x)
+# print(all_y)
+
+vis_length = max(all_x) - min(all_x) + 3
+vis_height = max(all_y) + 2
+sand_drop = 500 - min(all_x) + 1
+
+# print(vis_length)
+# print(vis_height)
+
+# Here we create our basic, empty visualization
+for y in range(vis_height):
+    visual.append([])
+    if y == 0:
+        for x in range(vis_length):
+            if x == sand_drop:
+                visual[-1].append('+')
+            else:
+                visual[-1].append('.')
+    elif y == vis_height - 1:
+        for x in range(vis_length):
+            visual[-1].append('_')
+    else:
+        for x in range(vis_length):
+            visual[-1].append('.')
+
+# Here we convert the rock_paths into numbers that correspond on
+#   our visualization
+temp_rock_paths = []
+for path in rock_paths:
+    new_path = []
+    for location in path:
+        location = eval('[' + location + ']')
+        location[0] = location[0] - min(all_x) + 1
+        new_path.append(location)
+    temp_rock_paths.append(new_path)
+rock_paths = temp_rock_paths
+
+# Now we paint on our rocks
+for path in rock_paths:
+    for i in range(1, len(path)):
+        diff = [(path[i][0]-path[i-1][0]), (path[i][1]-path[i-1][1])]
+        # print(diff)
+        
+        # First step is to paint the first rock, path[i-1]
+        visual[path[i-1][1]][path[i-1][0]] = '#'
+        # [print(' '.join(line)) for line in visual]
+        while diff != [0,0]:
+            # print(f'looping diff {diff}')
+            # Lateral movement
+            if diff[0] != 0:
+                # print('lateral')
+                if diff[0] > 0:
+                    diff[0] -= 1
+                    visual[path[i][1]][path[i-1][0]+diff[0]] = '#'
+                else:
+                    diff[0] += 1
+                    # print(f"Check point {path[i-1]} and diff {diff}")
+                    visual[path[i][1]][path[i-1][0]+diff[0]] = '#'
+            # Vertical movement
+            else:
+                # print('vertical')
+                if diff[1] > 0:
+                    diff[1] -= 1
+                    visual[path[i-1][1]+diff[1]][path[i][0]] = '#'
+                    # print(diff)
+                else:
+                    diff[1] += 1
+                    visual[path[i-1][1]+diff[1]][path[i][0]] = '#'
+                    # print(diff)
+            # [print(' '.join(line)) for line in visual]
+        visual[path[i][1]][path[i][0]] = '#'
+        # [print(' '.join(line)) for line in visual]
+
+
+[print(' '.join(line)) for line in visual]
+
+# Okay, whew. Now I'll work on the sand falling...
