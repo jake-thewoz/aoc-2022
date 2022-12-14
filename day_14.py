ae889d8a1b4cbcd6af6c3c183ad4488993f35eb9
Jakob <jake.thewoz@gmail.com>
2022-12-14 15:13:01 -0500

    Solved part one

--- day_14.py
+++ day_14.py
@@ -1,6 +1,8 @@
 import data_getter
+import os
+import time
 
-data = data_getter.get_data(144).splitlines()
+data = data_getter.get_data(14).splitlines()
 
 # [print(line) for line in data]
 
@@ -96,3 +98,57 @@ for path in rock_paths:
 [print(' '.join(line)) for line in visual]
 
 # Okay, whew. Now I'll work on the sand falling...
+
+def find_next(current):
+    # print(f"Evaluating {current}")
+    if visual[current[1]+1][current[0]] == '_':
+        print('Game over!')
+        visual[current[1]+1][current[0]] = 'O'
+        return [current[0],current[1]+1]
+    elif visual[current[1]+1][current[0]] == '.':
+        print('moving straight down')
+        return [current[0], current[1]+1]
+    elif visual[current[1]+1][current[0]] == 'o' or visual[current[1]+1][current[0]] == '#':
+        if visual[current[1]+1][current[0]-1] == '.':
+            print('moving down left')
+            return [current[0]-1, current[1]+1]
+        elif visual[current[1]+1][current[0]+1] == '.':
+            print('moving down right')
+            return [current[0]+1, current[1]+1]
+        else:
+            print('staying put')
+            return current
+
+game_on = True
+counter = 0
+
+while game_on:
+    counter += 1
+
+
+    # Here we make a sand kernel:
+
+    current_pos = [sand_drop, 0]
+    sand_falling = True
+
+    while sand_falling:
+        new_pos = find_next(current_pos)
+
+        if visual[new_pos[1]][new_pos[0]] == 'O':
+            sand_falling = False
+            game_on = False
+        elif new_pos == current_pos:
+            sand_falling = False
+            visual[new_pos[1]][new_pos[0]] = 'o'
+        else:
+            visual[new_pos[1]][new_pos[0]] = 'o'
+            visual[current_pos[1]][current_pos[0]] = '.'
+        
+        current_pos = new_pos
+            
+
+        # time.sleep(.02)
+        # os.system('cls')
+        # [print(' '.join(line)) for line in visual]
+    
+print(f"{counter-1} grains of sand have stabilized.")
