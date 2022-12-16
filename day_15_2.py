7ad164ffa8ce7927e15c65a6267f1d00fd3fdc4d
Jakob <jake.thewoz@gmail.com>
2022-12-16 15:58:18 -0500

    Replaced for loops with map and filter funcs

--- day_15_2.py
+++ day_15_2.py
@@ -10,9 +10,9 @@ beacons = []
 manhattan_distance = []
 
 for line in numbers:
-    sensors.append([int(line[0]), int(line[1])])
+    # manhattan distance is now the third int in the sensors list
+    sensors.append([int(line[0]), int(line[1]), abs(int(line[0]) - int(line[2])) + abs(int(line[1]) - int(line[3]))])
     beacons.append([int(line[2]), int(line[3])])
-    manhattan_distance.append(abs(int(line[0]) - int(line[2])) + abs(int(line[1]) - int(line[3])))
 
 impossible = set()
 # ROW = 21
@@ -22,27 +22,35 @@ ROW = 4000001
 # We'll just apply it row-by-row, until we find a space in our desired range
 #   that isn't impossible
 
+def find_impossible(sensor, spread):
+    combine_positive = lambda k: sensor[0] + k
+    combine_negative = lambda k: sensor[0] - k
+    response = set(map(combine_positive, range(1, spread+1)))
+    response |= set(map(combine_negative, range(1, spread+1)))
+    response.add(sensor[0])
+    return response
+
+
+def find_spreads(sensor, row):
+    return ((sensor[1] + sensor[2]) - row) if sensor[1] < row else (row - (sensor[1] - sensor[2]))
+
+
 for i in range(ROW):
     print(f"---Row {i}---")
-    for s in range(len(sensors)):
-        sensor = sensors[s]
-        reach = manhattan_distance[s]
 
-        # print(f'Checking for sensor {sensor}, with reach {reach}...')
+    sensor_list = list(filter(lambda sensor: \
+        (sensor[1] <= i and sensor[1] + sensor[2] >= i) \
+        or (sensor[1] >= i and sensor[1] - sensor[2] <= i),
+        sensors))
+    
+    rows = [i] * len(list(sensor_list))
+    spreads = list(map(find_spreads, sensor_list, rows))
+    impossible.update(*map(find_impossible, sensor_list, spreads))
 
-        if (sensor[1] <= i and sensor[1] + reach >= i) \
-            or (sensor[1] >= i and sensor[1] - reach <= i):
+    # print(list(sensor_list))
+    # print(list(spreads))
+    # print((impossible))
 
-            spread = ((sensor[1] + reach) - i) if sensor[1] < i else (i - (sensor[1] - reach))
-
-            # print(f'Hit: Applying sensor {sensor}, with spread {spread}')
-
-            # count = len(impossible)
-
-            impossible.add(sensor[0])
-            impossible |= set(map(lambda k: sensor[0] + k, range(1, spread+1)))
-            impossible |= set(map(lambda k: sensor[0] - k, range(1, spread+1)))
-        
     if len(set(range(ROW)) - impossible):
         diff = list(set(range(ROW)) - set(impossible))[0]
         answer = [diff, i]
